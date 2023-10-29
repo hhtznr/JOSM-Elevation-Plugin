2a80917d6681e4fda5db549a407dfdd988c0b623
hhtznr <57875126+hhtznr@users.noreply.github.com>
2023-10-29 10:18:06 +0100

    Add icon for displaying local elevation to status line of map frame

--- src/hhtznr/josm/plugins/elevation/ElevationPlugin.java
+++ src/hhtznr/josm/plugins/elevation/ElevationPlugin.java
@@ -1,10 +1,15 @@
 package hhtznr.josm.plugins.elevation;
 
+import hhtznr.josm.plugins.elevation.gui.LocalElevationLabel;
+
+import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.tools.Logging;
 
 public class ElevationPlugin extends Plugin {
+
+	private LocalElevationLabel localElevationLabel = null;
 
 	 /**
      * Initializes the plugin.
@@ -15,4 +20,19 @@ public class ElevationPlugin extends Plugin {
         Logging.info("Elevation plugin initialized");
     }
 
+    /**
+     * Called after Main.mapFrame is initialized. (After the first data is loaded).
+     * You can use this callback to tweak the newFrame to your needs, as example install
+     * an alternative Painter.
+     */
+    @Override
+    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+        super.mapFrameInitialized(oldFrame, newFrame);
+        if (localElevationLabel == null)
+            localElevationLabel = new LocalElevationLabel(newFrame);
+        else
+            localElevationLabel.addToMapFrame(newFrame);
+        if (newFrame != null)
+            localElevationLabel.setVisible(true);
+    }
 }
---
+++ src/hhtznr/josm/plugins/elevation/gui/LocalElevationLabel.java
@@ -0,0 +1,34 @@
+package hhtznr.josm.plugins.elevation.gui;
+
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapStatus;
+import org.openstreetmap.josm.gui.widgets.ImageLabel;
+import org.openstreetmap.josm.tools.GBC;
+
+/**
+ * An image label for the status line of a map frame, which is intended to
+ * display the elevation at the mouse pointer location on the map.
+ */
+public class LocalElevationLabel extends ImageLabel {
+
+    public LocalElevationLabel(MapFrame mapFrame) {
+        super("ele", "The terrain elevation at the mouse pointer.", 10, MapStatus.PROP_BACKGROUND_COLOR.get());
+
+        setForeground(MapStatus.PROP_FOREGROUND_COLOR.get());
+        setText("No data");
+        addToMapFrame(mapFrame);
+    }
+
+	/**
+	 * Adds this local elevation label to the status line of a map frame.
+	 *
+	 * @param mapFrame The JOSM map frame where this local elevation label should
+	 *                 be added to the status line.
+	 */
+    public void addToMapFrame(MapFrame mapFrame) {
+        // Add after the longitude ImageLabel at index = 2
+        // or alternatively at index 0 or 1, if index = 2 should be out of range
+        int index = Math.min(mapFrame.statusLine.getComponentCount(), 2);
+        mapFrame.statusLine.add(this, GBC.std().insets(3, 0, 0, 0), index);
+    }
+}
